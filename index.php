a2737d675806991538f51deb108b51ad7b7cde3f
PBrus <b_przemas@interia.pl>
2017-02-17 10:25:07 +0100

    Update welcome page

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/model.php';
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader);
+$state = True;
+$error = "None";
+
+$questfile = new File("questions.txt");
+if (!$questfile->readable())
+{
+	$state = False;
+	$error = $questfile->error();
+}
 
 echo $twig->render('index.html.twig', array(
-	
+		'state' => $state,
+		'error' => $error
 ));
