697a938b3a85edf6996f23304f92bbe9606c3e7c
PBrus <b_przemas@interia.pl>
2017-03-08 23:38:25 +0100

    Finish page with results

--- finish.php
+++ finish.php
@@ -11,17 +11,27 @@ $allQuestions = Session::getVar('allQuestions');
 $amountQuestions = Session::getVar('amountQuestions');
 $score = 0;
 $numberCorrectQuestions = 0;
+$maxScore = 0;
 
 for ($i = 0; $i < $amountQuestions; $i++) {
+    $maxScore += $allQuestions[$i]->weight();
     if ($allQuestions[$i]->evaluate() != 0) {
         $score += $allQuestions[$i]->evaluate();
         $numberCorrectQuestions++;
     }
 }
 
+$perScore = (int)(100 * $score / $maxScore);
+$perQuestions = (int)(100 * $numberCorrectQuestions / $amountQuestions);
+
 echo $twig->render('finish.html.twig', array(
         'user' => Session::getVar('user'),
         'score' => $score,
+        'maxScore' => $maxScore,
+        'perScore' => $perScore,
         'numberCorrectQuestions' => $numberCorrectQuestions,
-        'amountQuestions' => $amountQuestions
+        'amountQuestions' => $amountQuestions,
+        'perQuestions' => $perQuestions
 ));
+
+Session::stop();
